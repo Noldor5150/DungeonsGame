7fe86395847208bc12320b75e0a880efb462f949
Noldor5150 <ruikis.paulius@gmail.com>
2020-05-09 14:40:55 +0300

    implementing smart enemy movment, but not workin:(

--- DungeonsGame/Enemy.cs
+++ DungeonsGame/Enemy.cs
@@ -14,12 +14,20 @@ namespace DungeonsGame
         Timer timer;
         Point destination;
         Point enemyPlace;
+        State[,] map;
         Movement movement;
         int step = 3;
+        int[,] fmap;
+        int paths;
+        Point[] mileStone;
+        int pathStep;
 
-        public Enemy( PictureBox picEnemy, PictureBox[,]mapPic, State[,] map)
+
+        public Enemy(PictureBox picEnemy, PictureBox[,] mapPic, State[,] map)
         {
             enemy = picEnemy;
+            this.map = map;
+            fmap = new int[map.GetLength(0), map.GetLength(1)];
             movement = new Movement(picEnemy, mapPic, map);
             enemyPlace = movement.MyNowPoint();
             destination = new Point(15, 7);
@@ -31,16 +39,16 @@ namespace DungeonsGame
         {
             timer = new Timer();
             timer.Interval = 100;
-            timer.Tick += Timer_Tick;    
+            timer.Tick += Timer_Tick;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             if (enemyPlace == destination)
-            
+
                 return;
-                MoveEnemy(destination);
-            
+            MoveEnemy(destination);
+
         }
 
         private void MoveEnemy(Point newPlace)
@@ -61,10 +69,113 @@ namespace DungeonsGame
             }
             else
             {
-                y = enemyPlace.Y - newPlace.Y < step ? newPlace.Y - enemyPlace.Y : -step;   
+                y = enemyPlace.Y - newPlace.Y < step ? newPlace.Y - enemyPlace.Y : -step;
             }
             movement.MoveByStep(x, y);
             enemyPlace = movement.MyNowPoint();
+        }
+
+        private bool FindPath()
+        {
+
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    fmap[x, y] = 0;
+                }
+            }
+            bool pathAdded;
+            bool pathFound = false;
+            fmap[enemyPlace.X, enemyPlace.Y] = 1;
+            int pathNumber = 1;
+            do
+            {
+                pathAdded = false;
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    for (int y = 0; y < map.GetLength(1); y++)
+                    {
+                        if (fmap[x, y] == pathNumber)
+                        {
+                            MarkPath(x + 1, y, pathNumber + 1);
+                            MarkPath(x - 1, y, pathNumber + 1);
+                            MarkPath(x, y - 1, pathNumber + 1);
+                            MarkPath(x, y + 1, pathNumber + 1);
+                            pathAdded = true;
+                        }
+                    }
+                }
+                if (fmap[destination.X, destination.Y] > 0)
+                {
+                    pathFound = true;
+                    break;
+                }
+                pathNumber++;
+            } while (pathAdded);
+            if (!pathFound)
+            {
+                return false;
+            }
+            int sx = destination.X;
+            int sy = destination.Y;
+            paths = pathNumber;
+            while (pathNumber >= 0)
+            {
+                mileStone[pathNumber].X = sx;
+                mileStone[pathNumber].X = sy;
+
+                if (IsItLegitPath(sx + 1, sy, pathNumber))
+                {
+                    sx++;
+                }
+                else if (IsItLegitPath(sx - 1, sy, pathNumber))
+                {
+                    sx--;
+                }
+                else if (IsItLegitPath(sx, sy + 1, pathNumber))
+                {
+                    sy++;
+                }
+                else if (IsItLegitPath(sx, sy - 1, pathNumber))
+                {
+                    sy--;
+                }
+                pathNumber--;
+            }
+            pathStep = 0;
+            return true;
+        }
+
+        private void MarkPath(int x, int y, int pathNumber)
+        {
+            if (x < 0 || x >= map.GetLength(0))
+            {
+                return;
+            }
+            if (y < 0 || y >= map.GetLength(1))
+            {
+                return;
+            }
+            if (fmap[x, y] > 0)
+            {
+                return;
+            }
+            fmap[x, y] = pathNumber;
+        }
+
+        private bool IsItLegitPath(int x, int y, int pathNumber)
+        {
+            if (x < 0 || x >= map.GetLength(0))
+            {
+                return false;
+
+            }
+            if (y < 0 || y >= map.GetLength(1))
+            {
+                return false;
+            }
+          return fmap [x,y] == pathNumber;
         }
     }
 }
